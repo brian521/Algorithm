721f446ad1c8e52ddda9c0329a14b5dfa4a64910
brian521 <59503208+brian521@users.noreply.github.com>
2024-02-09 14:54:02 +0900

    Create 기찍N.cpp

---
+++ c++/2742.기찍N/기찍N.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long N;
+    
+    cin >> N;
+    
+    while(N)
+    {
+        cout << N-- << "\n";
+    }
+    
+    return 0;
+}
